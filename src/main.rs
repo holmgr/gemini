e1fc0022d20be98f8bd7afb11f83ab79a7dafa25
holmgr <viktor.holmgren@gmail.com>
2017-10-18 21:50:08 +0200

    Implement generic compile time resource handler

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #[macro_use]
 extern crate serde_derive;
+extern crate serde_json;
 extern crate serde;
 extern crate preferences;
 extern crate notify;
@@ -8,12 +9,23 @@ extern crate petgraph;
 
 mod game_config;
 mod markov;
+mod resources;
 
 fn main() {
 
     // Load GameConfig from disk
     let mut config = game_config::GameConfig::retrieve();
     println!("Initial config is: {:?}", config);
+
+    let fac = resources::ResourceHandler::new();
+    let astro = fac.fetch_resource::<resources::AstronomicalNamesResource>()
+        .unwrap();
+
+    let mut markov = markov::MarkovChain::new(&astro.names, 42);
+
+    for _ in 0..10 {
+        println!("{}", markov.generate());
+    }
 
     // Reload GameConfig if file on disk changes
     loop {
---
+++ src/resources.rs
@@ -0,0 +1,69 @@
+use std::str;
+use serde_json;
+use serde::de::Deserialize;
+use std::collections::HashMap;
+
+/// Generic Resource trait to be implemented by all Resource types which should
+/// be loaded at compile time
+/// KEY must be unique to the specific resouce (e.g the filename of the
+/// resource)
+pub trait Resource: Deserialize {
+    const KEY: &'static str;
+}
+
+/// Resource factory which holds all resources serialized based on the Resource
+/// KEY.
+pub struct ResourceHandler {
+    resources: HashMap<&'static str, &'static str>,
+}
+
+impl ResourceHandler {
+    /// Creates a new ResourceHandler, initilaizing the map of resources
+    pub fn new() -> ResourceHandler {
+
+        // Init map of resources to be included
+        let mut resources = HashMap::new();
+        resources.insert(
+            AstronomicalNamesResource::KEY,
+            include_str!("../res/astronomical_names.json"),
+        );
+
+        ResourceHandler { resources }
+    }
+
+    /// Attempts to returns the resource with the given type, will return None
+    /// if the type has no resource or if the deserialization fails
+    pub fn fetch_resource<T: Resource>(&self) -> Option<T> {
+        self.resources.get(T::KEY).and_then(|res: &&str| {
+            serde_json::from_str(res).unwrap_or(None)
+        })
+    }
+}
+
+#[derive(Serialize, Deserialize, PartialEq, Debug)]
+/// Resource of all training names for name generation of celestial objects
+pub struct AstronomicalNamesResource {
+    pub names: Vec<String>,
+    pub greek: Vec<String>,
+    pub decorators: Vec<String>,
+}
+
+impl Resource for AstronomicalNamesResource {
+    const KEY: &'static str = "astronomical_names";
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_fetch_resource() {
+        let factory = ResourceHandler::new();
+        let res = factory
+            .fetch_resource::<AstronomicalNamesResource>()
+            .unwrap();
+        assert!(res.names.len() > 0);
+        assert!(res.greek.len() > 0);
+        assert!(res.decorators.len() > 0);
+    }
+}
