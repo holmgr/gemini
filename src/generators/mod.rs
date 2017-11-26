90f9f0fa63c21e6b878cc06a421a8d50692da4d9
holmgr <viktor.holmgren@gmail.com>
2017-11-26 22:40:17 +0100

    Parallelize system generation

--- src/generators/mod.rs
+++ src/generators/mod.rs
@@ -1,13 +1,15 @@
 use rand::SeedableRng;
 use std::time::Instant;
+use rayon::prelude::*;
 use rand::isaac::IsaacRng;
 use statrs::distribution::{Distribution, Uniform, Normal};
 use nalgebra::geometry::Point3 as Point;
 
+pub mod names;
+
 use astronomicals::{Galaxy, System};
 use game_config::GameConfig;
 
-pub mod names;
 
 /// Generic Generator trait to be implemented by concrete generators of different kinds.
 pub trait Gen {
@@ -30,46 +32,62 @@ pub fn generate_galaxy(config: &GameConfig) -> Galaxy {
     let new_seed: &[_] = &[config.map_seed];
     let mut rng: IsaacRng = SeedableRng::from_seed(new_seed);
 
+    // Measure time for generation
     let now = Instant::now();
 
-    // Generate clusters
     // Clusters are spaced uniformly, systems gaussian
-    let mut systems = Vec::<System>::new();
-
     let cluster_loc_gen = Uniform::new(0., config.galaxy_size).unwrap();
     let system_loc_gen = Normal::new(config.cluster_size_mean, config.cluster_size_std).unwrap();
 
-    // TODO: Generate systems for each cluster in parallel
+    // Generate clusters
+    let mut clusters = vec![];
     for _ in 0..config.number_of_clusters {
-        let cluster_pos = Point::new(
-            cluster_loc_gen.sample::<IsaacRng>(&mut rng),
-            cluster_loc_gen.sample::<IsaacRng>(&mut rng),
-            cluster_loc_gen.sample::<IsaacRng>(&mut rng),
-        );
-
-        // Add randomness to cluster size
-        let cluster_size = system_loc_gen.sample::<IsaacRng>(&mut rng) as u64;
-
-        // Generate x,y,z generators based at cluster location
-        let norm_x = Normal::new(cluster_pos.coords.x, config.cluster_spread).unwrap();
-        let norm_y = Normal::new(cluster_pos.coords.y, config.cluster_spread).unwrap();
-        let norm_z = Normal::new(cluster_pos.coords.z, config.cluster_spread).unwrap();
-
-        // Generate systems
-        for _ in 0..cluster_size {
-            systems.push(System::new(Point::new(
-                norm_x.sample::<IsaacRng>(&mut rng),
-                norm_y.sample::<IsaacRng>(&mut rng),
-                norm_z.sample::<IsaacRng>(&mut rng),
-            )));
-        }
+        clusters.push((
+            Point::new(
+                cluster_loc_gen.sample::<IsaacRng>(&mut rng),
+                cluster_loc_gen.sample::<IsaacRng>(&mut rng),
+                cluster_loc_gen.sample::<IsaacRng>(&mut rng),
+            ),
+            system_loc_gen.sample::<IsaacRng>(&mut rng) as u64,
+        ))
     }
 
+    // Generate systems for each cluster in parallel
+    // Fold will generate one vector per thread (per cluster), reduce will
+    //combine them to the final result
+    let systems = clusters
+        .into_par_iter()
+        .fold(|| Vec::<System>::new(), |mut cluster_systems: Vec<System>,
+         (cluster_pos, cluster_size)| {
+            // Generate x,y,z generators based at cluster location
+            let norm_x = Normal::new(cluster_pos.coords.x, config.cluster_spread).unwrap();
+            let norm_y = Normal::new(cluster_pos.coords.y, config.cluster_spread).unwrap();
+            let norm_z = Normal::new(cluster_pos.coords.z, config.cluster_spread).unwrap();
+
+            // TODO: Do something smarter than cloning state of generator,
+            // since all clusters will be generated identically now
+            let mut rng = rng.clone();
+
+            // Generate systems
+            for _ in 0..cluster_size {
+                cluster_systems.push(System::new(Point::new(
+                    norm_x.sample::<IsaacRng>(&mut rng),
+                    norm_y.sample::<IsaacRng>(&mut rng),
+                    norm_z.sample::<IsaacRng>(&mut rng),
+                )));
+            }
+            cluster_systems
+        })
+        .reduce(|| Vec::<System>::new(), |mut systems, subsystems| {
+            systems.extend(subsystems);
+            systems
+        });
+
     info!(
-        "Generated new galaxy containing: {} clusters and {} systems taking {} seconds",
-        config.cluster_size_mean,
+        "Generated new galaxy containing: {} clusters and {} systems taking {} ms",
+        config.number_of_clusters,
         systems.len(),
-        now.elapsed().as_secs()
+        ((now.elapsed().as_secs() * 1_000) + (now.elapsed().subsec_nanos() / 1_000_000) as u64)
     );
     Galaxy::new(systems)
 }
