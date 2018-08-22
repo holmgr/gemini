9b20f4e02ec349fab8f2ede124b87e5ce94014f0
holmgr <viktor.holmgren@gmail.com>
2018-08-22 15:14:40 +0200

    Switch to hashmap to store market agents

--- src/economy/market.rs
+++ src/economy/market.rs
@@ -1,27 +1,31 @@
-use astronomicals::System;
+use std::collections::HashMap;
 
 use super::*;
+use astronomicals::System;
+use utils::Point;
 
 /// Controls an economic market, i.e a sector of trading systems.
 #[derive(Serialize, Deserialize)]
 pub struct Market {
-    agents: Vec<Agent>,
+    agents: HashMap<Point, Agent>,
 }
 
 impl Market {
     /// Creates a new empty market.
     pub fn new() -> Self {
-        Market { agents: vec![] }
+        Market {
+            agents: HashMap::new(),
+        }
     }
 
     /// Returns the agent, if any, which is associated with the given system.
-    pub fn agent(&self, system_hash: u32) -> Option<Agent> {
-        unimplemented!();
+    pub fn agent(&self, location: &Point) -> Option<&Agent> {
+        self.agents.get(location)
     }
 
     /// Adds the given system to this market.
     pub fn add_system(&mut self, system: &System) {
-        self.agents.push(Agent::new(&system));
+        self.agents.insert(system.location, Agent::new(&system));
     }
 }
 
