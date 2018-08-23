252fe76dd0620bdf760ec5bd6e5ab3a04b8f5568
holmgr <viktor.holmgren@gmail.com>
2018-08-23 11:06:09 +0200

    Build adjecency list (trade_graph) of neighboring systems in every market

--- src/economy/market.rs
+++ src/economy/market.rs
@@ -4,15 +4,32 @@ use super::*;
 use astronomicals::System;
 use utils::Point;
 
+static MAX_TRADE_DISTANCE: f64 = 20.;
+
 /// Controls an economic market, i.e a sector of trading systems.
 #[derive(Serialize, Deserialize)]
 pub struct Market {
     agents: HashMap<Point, Agent>,
+    neighbors: HashMap<Point, Vec<Point>>,
 }
 
 impl Market {
     /// Creates a market with the given systems and population.
     pub fn new(systems: Vec<&System>, population: u64) -> Self {
+        // Build an adjecency list of all neighboring systems needed for trade resolving.
+        let mut neighbors = HashMap::new();
+        for i in 0..systems.len() {
+            let mut point_neighbors = vec![];
+            for j in 0..systems.len() {
+                if i != j && systems[i].location.distance(&systems[j].location) < MAX_TRADE_DISTANCE
+                {
+                    point_neighbors.push(systems[j].location);
+                }
+            }
+
+            neighbors.insert(systems[i].location, point_neighbors);
+        }
+
         let system_count = systems.len() as u64;
         let agents = HashMap::from_iter(systems.into_iter().map(|system| {
             (
@@ -20,7 +37,7 @@ impl Market {
                 Agent::new(system, population / system_count),
             )
         }));
-        Market { agents }
+        Market { agents, neighbors }
     }
 
     /// Returns the agent, if any, which is associated with the given system.
