23e9359c1e03172b3594a536a630190d8972b950
holmgr <viktor.holmgren@gmail.com>
2018-12-03 11:58:02 +0100

    Make all components renderable

--- src/ui/component/mod.rs
+++ src/ui/component/mod.rs
@@ -1,3 +1,13 @@
 use super::*;
 
-pub trait Component: Renderable {}
+/// A renderable component.
+pub trait Component {
+    fn render(&self, area: RenderArea, ctx: &mut RenderContext);
+}
+
+impl<T> Renderable for T
+where T: Component {
+    fn render(&self, area: RenderArea, ctx: &mut RenderContext) {
+        Component::render(self, area, ctx);
+    }
+}
